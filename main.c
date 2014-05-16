f3223de2f2a19b4786c85c46b60b60b7e5652788
bkoch <benni_koch@gmx.de>
2014-05-16 12:10:47 +0200

    Removed unnecessary comment.

--- main.c
+++ main.c
@@ -1,16 +1,4 @@
-/*
-**
-**                           Main.c
-**
-**
-**********************************************************************/
-/*
-   Last committed:     $Revision: 00 $
-   Last changed by:    $Author: $
-   Last changed date:  $Date:  $
-   ID:                 $Id:  $
 
-**********************************************************************/
 #include "stm32f10x_conf.h"
 #include "tlc.h"
 #include "main.h"
